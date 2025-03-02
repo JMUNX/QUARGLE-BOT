91e70e8ed236ee9a59590b2ced894cad84edc375
MUNX <m.e.xunx@gmail.com>
2025-03-02 13:15:33 -0800

    Changed Version Information

--- QUARGLE-V5.py
+++ QUARGLE-V5.py
@@ -75,7 +75,7 @@ async def on_ready():
     logger.info(f"Bot is online as {bot.user.name}")
     channel = bot.get_channel(1345184113623040051)
     if channel:
-        version = "69.420.8"
+        version = "69.420.10"
         embed = discord.Embed(
             title="Quargle is online",
             description=f"{version} is now live",
