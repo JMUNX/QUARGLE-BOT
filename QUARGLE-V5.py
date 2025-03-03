20145b98db81fa1729a028cd198dd13f44fb2ab6
MUNX <m.e.xunx@gmail.com>
2025-03-02 20:33:25 -0800

    Final update for tonight

--- QUARGLE-V5.py
+++ QUARGLE-V5.py
@@ -81,7 +81,7 @@ async def on_ready():
     logger.info(f"Bot is online as {bot.user.name}")
     channel = bot.get_channel(1345184113623040051)
     if channel:
-        version = "69.420.42"
+        version = "69.420.45"
         embed = Embed(
             title="Quargle is online",
             description=f"{version} is now live",
